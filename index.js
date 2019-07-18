58d02e1a7ba4b29545bddfafa0676728d7cbadf9
Tawhid Ali <tawhidali126@gmail.com>
2019-07-18 21:44:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(myDeliLine, name) {
-  let inline = myDeliLine;  //current people in                              line
-  let person = name;    // person who will be                              inline
+  let inline = myDeliLine;  //current people in line
+  let person = name;    // person who will be inline
   inline.push(person);
   
   let greeting =  `Welcome, ${person}. You are number ${inline.length} in line.`;
@@ -42,7 +42,7 @@ function nowServing(myDeliLine) {
   
   const noOneHere = 'There is nobody waiting to be served!';
   let comeOnUp = `Currently serving ${inline[0]}.`;
-  inline.shift();           //delete first person             //inline after they have been called
+  inline.shift();           //delete first person inline after they have been called
   
   if(inline.length <= 0) {
     return noOneHere;
